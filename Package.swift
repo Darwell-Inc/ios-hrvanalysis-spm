be378f10cf23c5d419bafa1cb9c637eca2f83891
Ivan O <ioparin@pm.me>
2022-08-26 18:29:46 +0400

    adding workaround for xcode/spm bug

--- Package.swift
+++ Package.swift
@@ -1,7 +1,9 @@
-// swift-tools-version:5.5
+// swift-tools-version:5.6
 
 import PackageDescription
 
+#warning("move binary artifact to private hosting")
+#warning("core-ios-spm is public due to a bug is SPM/Xcode that prevents authentication")
 let package = Package(
     name: "darwell-core",
     platforms: [
@@ -10,7 +12,9 @@ let package = Package(
     products: [
         .library(
             name: "DarwellCore",
-            targets: ["DarwellCore"]
+            targets: [
+                "DarwellCore",
+            ]
         ),
     ],
     targets: [
