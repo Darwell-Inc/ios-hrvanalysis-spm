f390d8b57c51bc39216441be796897de36c62e8b
Ivan O <ioparin@pm.me>
2022-08-29 00:15:50 +0400

    updating binary target to dynamic framework

--- Package.swift
+++ Package.swift
@@ -20,8 +20,8 @@ let package = Package(
     targets: [
        .binaryTarget(
             name: "DarwellCore",
-            url: "https://github.com/galen-it/core-ios-spm/releases/download/1.0.0/DarwellCore.xcframework.zip",
-            checksum: "83e879c2b94bea92869053f2857b6526225d43f227857d8ced5dd385eed6674a"
+            url: "https://github.com/galen-it/core-ios-spm/releases/download/1.0.1/DarwellCore.xcframework.zip",
+            checksum: "2060068a8859a9973cc10e63de87ca965ef6d0239f5ee5dd0be5e813241677a1"
         )
     ]
 )
