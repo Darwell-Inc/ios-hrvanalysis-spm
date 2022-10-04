f91df9f4ebd9756bb82f962754fd3a39ccb92f3b
Ivan O <ioparin@pm.me>
2022-10-04 13:20:27 +0400

    updating binary artifact; squats detection

--- Package.swift
+++ Package.swift
@@ -20,8 +20,8 @@ let package = Package(
     targets: [
        .binaryTarget(
             name: "DarwellCore",
-            url: "https://github.com/galen-it/core-ios-spm/releases/download/1.2.0/DarwellCore.xcframework.zip",
-            checksum: "b03061a116814ef1af5e1a6544100660ae75ad27b42f77b417ee472c8248bb75"
+            url: "https://github.com/galen-it/core-ios-spm/releases/download/1.3.0/DarwellCore.xcframework.zip",
+            checksum: "071f8827e63b16584ef1a53ac5b3bec2c88085a4d0d9a919b98b3c4d0c3b0333"
         )
     ]
 )
